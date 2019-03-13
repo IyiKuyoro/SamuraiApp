7da66239ccf4d4fb766816ab8eb7d5721745e6f7
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-03-13 14:56:10 +0100

    setup data project

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = IYI-KUYORO\\MSSQLSERVER01; Database = SamuraiData; Trusted_Connection = True; ");
+        }
+    }
+}
